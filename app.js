18af138b2dd2a31c5bb6783826b81879f84a49c1
Pablo Schonwiesner <pabloschonwiesner@gmail.com>
2022-07-11 10:01:10 -0300

    Remove public server

--- app.js
+++ app.js
@@ -4,31 +4,21 @@ const cors = require('cors')
 require('./services/utils/env')
 const { error404, generalErrorHandler, auth0Error } = require('./middlewares/errors')
 const { checkJwt, decodeJsonWebToken } = require('./middlewares/auth')
-let history = require('connect-history-api-fallback');
 const indexRoutes = require('./routes')
 
 const app = express()
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-
-app.use('/public', express.static(path.join(__dirname, "/public/")));
 app.use(checkJwt)
 app.use(decodeJsonWebToken)
 app.use(auth0Error)
+
+app.use('/api', indexRoutes)
+
 app.use(error404)
 app.use(generalErrorHandler)
 
-app.use(history({
-  index: '/'
-}))
-
-
-app.get('/', function (req, res) {
-  res.sendFile('index.html', { root: path.join(__dirname, '/public/') });
-});
-
-app.use('/api', indexRoutes)
 
 
 module.exports = app
--- www.js
+++ www.js
@@ -5,14 +5,6 @@
   const { logger, loggerCO } = require('./services/logging/logger')
   const { PORT } = require('./config/config')
 
-  const https = require('https');
-  const fs = require('fs');
-
-  const options = {
-    key: fs.readFileSync('localhost-key.pem'),
-    cert: fs.readFileSync('localhost.pem')
-  };
-
   try {
     if (cluster.isMaster) {
       for (let i = 0; i < numCPUs; i++) {
@@ -21,10 +13,9 @@
     } else {
       app.set('port', PORT || 3000);
       const server = require('http').createServer(app)
-      https.createServer(options, app).listen(3333);
       // const realtime = require('./services/realtime/realtime')
 
-      server.listen(PORT, () => loggerCO.trace(`Servidor en el puerto ${PORT}. Servidor https en el puerto 3333`))
+      server.listen(PORT, () => loggerCO.trace(`Servidor en el puerto ${PORT}.`))
       server.on('error', (err) => logger.error(err))
 
       // si uso socket descomentar la siguiente linea
